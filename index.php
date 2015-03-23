a1c164286e78e655abce62bd4d5aa80797de1150
darree <darree@gmail.com>
2015-03-23 16:40:04 +0100

    Revert "Revert "qq""
    This reverts commit 8af42281d8b906edb56cc540dde9ea0ae3c509cd.

--- index.php
+++ index.php
@@ -1,4 +1,5 @@
 <?php
+require_once 'includes/class-query.php';
 /*if(!empty($_GET)) {
     $post = $_GET['p'];
     $cat = $_GET['cat'];
@@ -8,9 +9,27 @@ $post = filter_input(INPUT_GET, 'p');
 $cat = filter_input(INPUT_GET, 'cat');
 
 if(empty($post) && empty($cat)){
-    echo 'Home';
+    $post_array = $query->all_posts();
 }elseif(!empty($post)){
-    echo 'Single';
+    $post_array = $query->post($post);
 }elseif(!empty($cat)){
     echo 'Catalog';
 }
+?>
+
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title></title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
+
+</head>
+<body>
+    <?php foreach($post_array as $post): ?>
+    <div class="post">
+        <h1><a href="?p=<?php echo $post->ID ?>"><?php echo $post->post_title; ?>  </a></h1>
+        <p><?php  echo $post->post_content;  ?></p>
+    </div>
+    <?php endforeach; ?>
+</body>
+</html>
